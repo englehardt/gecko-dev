7e50f37266dbcc7150af5a55333eb16776a4b530
englehardt <englehardt@gmail.com>
2016-06-10 15:01:56 -0700

    Bugfix to get browser to compile, tracking protection (normal mode) doesn't seem to work. Enabling adbox causes the browser to stall and crash

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -7688,6 +7688,7 @@ nsHttpChannel::SetDoNotTrack()
                            NS_LITERAL_CSTRING("1"),
                            false);
   }
+}
 
 NS_IMETHODIMP nsHttpChannel::StartRedirectChannelInSandbox()
 {
@@ -7714,7 +7715,8 @@ NS_IMETHODIMP nsHttpChannel::StartRedirectChannelInSandbox()
     LOG(("nsHttpChannel::%s [this=%p] newChannel = %p\n", __FUNCTION__, this,
          newChannel.get()));
 
-    rv = SetupReplacementChannel(mURI, newChannel, true);
+    uint32_t redirectFlags = nsIChannelEventSink::REDIRECT_INTERNAL;
+    rv = SetupReplacementChannel(mURI, newChannel, true, redirectFlags);
     if (NS_FAILED(rv)) return rv;
 
     // Add LOAD_ANONYMOUS
